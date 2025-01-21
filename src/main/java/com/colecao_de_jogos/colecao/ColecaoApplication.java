37e120688989788d47f47e6cc4ddb7428f4382d4
murilovers <murilopiresandradecruz27@gmail.com>
2025-01-20 22:22:24 -0300

    alteraçoes

---
+++ src/main/java/com/colecao_de_jogos/colecao/ColecaoApplication.java
@@ -0,0 +1,13 @@
+package com.colecao_de_jogos.colecao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColecaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColecaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/colecao_de_jogos/colecao/ColecaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colecao_de_jogos.colecao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColecaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
